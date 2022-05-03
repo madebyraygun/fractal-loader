442452470748a2c4764c78d74a18aae7f8b05cb1
Dalton Rooney <dalton@madebyraygun.com>
2022-05-03 14:15:49 -0700

    feat: add translation file

---
+++ src/translations/en/fractal.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Fractal loader plugin for Craft CMS 4.x
+ *
+ * @link      https://madebyraygun.com/
+ * @copyright Copyright (c) 2022 Raygun Design, LLC
+ */
+
+/**
+ * Fractal en Translation
+ *
+ * Returns an array with the string to be translated (as passed to `Craft::t('fractal', '...')`) as
+ * the key, and the translation as the value.
+ *
+ * http://www.yiiframework.com/doc-2.0/guide-tutorial-i18n.html
+ *
+ * @author Raygun Design, LLC. <dev@madebyraygun.com>
+ * @package Fractal Loader
+ * @since 4.0.0
+ */
+return [
+    'Fractal loader plugin loaded' => 'Fractal loader plugin loaded',
+];
